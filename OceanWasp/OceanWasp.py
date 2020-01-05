f6d8b863e4cda3547f4935069130987fca06aea8
root <root@aziza.localdomain>
2020-01-04 20:18:21 -0500

    Added a text output

--- OceanWasp/OceanWasp.py
+++ OceanWasp/OceanWasp.py
@@ -36,30 +36,52 @@ def validate_input(args) -> ip_address:
         sys.exit()
 
     #Input check file 
-    if args.output:
-        if Path(args.output).is_dir():
+    if args.markdown:
+        if Path(args.markdown).is_dir():
             print(err_msg("Given argument is a path and not a file"))
             sys.exit()
 
     return ip
 
-def create_md_table(scanner : PortScanner) -> Tuple[list, list]:
+def scanner_to_table(scanner : PortScanner) -> Tuple[list, list, list]:
     columns = ["Host", "Port", "Service Name", "Product", "Version", "Extra Info", "Platform Enumeration"]
     full_table = []
 
+    all_hosts = []
+
     for scanned_host in scanner.all_hosts():
+        data = dict()
+        data['host'] = scanned_host
+
         if 'tcp' in scanner[scanned_host].keys():
+            #add space for open ports information
+            data['open_ports'] = list()
+
             for port, info in scanner[scanned_host]['tcp'].items():
                 if info['state'] == 'open':
+                    port_info = dict()
+
                     serv_name = info['name']
                     serv_prod = info['product']
                     serv_ver = info['version']
                     serv_extra = info['extrainfo']
                     serv_cpe = info['cpe']
-
+                    
+                    #create table
                     full_table.append([scanned_host, str(port), serv_name, serv_prod, serv_ver, serv_extra, serv_cpe])
 
-    return columns, full_table
+                    #create dictionairy
+                    port_info = {'Port' : port,
+                                 'Service Name' : serv_name,
+                                  'Product' : serv_prod, 
+                                  'Version' : serv_ver, 
+                                  "Extra Info" : serv_extra, 
+                                  "Platform Enumeration" : serv_cpe }
+                    data['open_ports'].append(port_info)
+
+        all_hosts.append(data)
+
+    return columns, full_table, all_hosts
 
 def render_md_table(columns: list, full_table: list) -> str:
     return Table(columns, full_table).render()
@@ -68,12 +90,30 @@ def render_md_table(columns: list, full_table: list) -> str:
 def render_tab_table(columns: list, full_table: list) -> str:
     return tabulate(full_table, headers=columns, tablefmt="fancy_grid")
 
+def render_text_info(data: list) -> str:
+    output_string = ""
+
+    for item in data:
+        output_string += "Host : {0}\n".format(item['host'])
+        output_string += "\nOpen Ports\n"
+
+        for port_info in item['open_ports']:
+            output_string += "\tPort : {0}\n".format(port_info['Port']) 
+            output_string += "\tService Name: {0}\n".format(port_info['Service Name']) 
+            output_string += "\tProduct : {0}\n".format(port_info['Product']) 
+            output_string += "\tVersion : {0}\n".format(port_info['Version']) 
+            output_string += "\tExtra Info : {0}\n".format(port_info['Extra Info']) 
+            output_string += "\tPlatform Enumeration : {0}\n".format(port_info['Platform Enumeration']) 
+
+    return output_string
+
 def main():
     print("Executing OceanWasp version %s." % __version__)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("target_host", help="IP address for target.")
-    parser.add_argument("-o", "--output", help="File to append data.")
+    parser.add_argument("--markdown", help="Markdown File to append data.")
+    parser.add_argument("--text", help="Text File to append data.")
     args = parser.parse_args()
 
     ip = validate_input(args)
@@ -90,17 +130,28 @@ def main():
         sys.exit()
 
     #create column and output data
-    columns, table = create_md_table(scanner)
+    columns, table, data_dict  = scanner_to_table(scanner)
 
     #if Output file given then write output to it
-    if args.output:
+    if args.markdown:
+        print(msg("Writing markdown to file"))
         md_table = render_md_table(columns, table)
 
-        with open(args.output, "a+") as output_file:
-            output_file.write("\n")
-            output_file.write(md_table)
+        with open(args.markdown, "a+") as markdown_file:
+            markdown_file.write("\n")
+            markdown_file.write(md_table)
+            
+    #if text argument given then output to text file
+    if args.text:
+        print(msg("Writing scan results to text file"))
+        text = render_text_info(data_dict)
+
+        with open(args.text, "a+") as text_file:
+            text_file.write("\n")
+            text_file.write(text)
 
     print(msg("Scan Results"))
     tabulate_table = render_tab_table(columns, table)
 
     print(tabulate_table)
+
